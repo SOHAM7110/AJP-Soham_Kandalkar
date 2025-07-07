78e033cf5a05918ece4a922d9fc562f27aab8b26
SOHAM_KANDALKAR <116878767+SOHAM7110@users.noreply.github.com>
2025-07-07 16:57:04 +0530

    Delete java-basics directory

--- java-basics/class.java
+++
@@ -1,9 +0,0 @@
-
-class a_Person{
-    static String name = "Soham";
-    static int age = 21;
-
-    public static void main(String agrs[]){
-        System.out.println("Welcome " + name + " " + age );
-    }
-}
