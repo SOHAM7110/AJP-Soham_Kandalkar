aca8bb87128b0ec62946cc2999fc050d789d0077
SOHAM_KANDALKAR <116878767+SOHAM7110@users.noreply.github.com>
2025-07-07 17:00:43 +0530

    Create 3_Objects.java

---
+++ java-basic/3_Objects.java
@@ -0,0 +1,26 @@
+class object {
+    /*
+     * WAP to calculate following things:
+     * a. sum of three numbers
+     * b. area of circle
+     */
+
+    public void sum(float a, float b, float c) {
+        float sum = a + b + c;
+        System.out.println("sum = " + sum);
+    }
+
+    public void area(double rad) {
+        double area = 3.14 * rad * rad;
+        // for float : use 3.14f
+        System.out.println("area = " + area);
+    }
+
+    public static void main(String[] args) {
+        object sum1 = new object();
+        sum1.sum(2, 6, 4);
+
+        object area1 = new object();
+        area1.area(2);
+    }
+}
