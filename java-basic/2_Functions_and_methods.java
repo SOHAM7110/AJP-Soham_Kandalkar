039f1d7573c62930f128f486d83420e2295a18b2
SOHAM_KANDALKAR <116878767+SOHAM7110@users.noreply.github.com>
2025-07-07 17:00:09 +0530

    Create 2_Functions_and_methods.java

---
+++ java-basic/2_Functions_and_methods.java
@@ -0,0 +1,16 @@
+class b_Functions_n_Methods{
+    //variables:
+    static String name = "Soham";
+    static int age = 21;
+
+    //function:
+
+    public static void display(){
+        System.out.println("Welcome " + name + " " + age);
+    }
+
+    //main:
+    public static void main(String[] args){
+        display();
+    }
+}
