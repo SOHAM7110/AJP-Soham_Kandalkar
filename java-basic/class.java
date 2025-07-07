78a8d477953e8d6d868f44e522985c41d91239b3
SOHAM_KANDALKAR <116878767+SOHAM7110@users.noreply.github.com>
2025-07-07 16:57:25 +0530

    Create class.java

---
+++ java-basic/class.java
@@ -0,0 +1,9 @@
+
+class a_Person{
+    static String name = "Soham";
+    static int age = 21;
+
+    public static void main(String agrs[]){
+        System.out.println("Welcome " + name + " " + age );
+    }
+}
