7e4fce42b0a9f23fdd134c85a6f8196190239e82
SOHAM_KANDALKAR <116878767+SOHAM7110@users.noreply.github.com>
2025-07-07 16:51:12 +0530

    Delete 1_person.java

--- 1_person.java
+++
@@ -1,9 +0,0 @@
-class Person{
-    static String name = "Soham";
-    static int age = 21;
-
-    public static void main(String agrs[]){
-        System.out.println(name);
-        System.out.println(age);
-    }
-}
